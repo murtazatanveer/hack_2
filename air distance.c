b9149e3caa5a431789e624ca7b704d035bab0f48
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-10-20 20:01:12 +0500

    3rd

--- air distance.c
+++ air distance.c
@@ -20,6 +20,13 @@ scanf("%lf",&lat_of_des);
 printf("\n\nLongitude : ");
 scanf("%lf",&lon_of_des);
 
+// i convert it in radians to use in formula 
+
+double a = (lat_of_ori*pi)/180;
+
+double b = (lon_of_ori*pi)/180;
+
+
 
 
 
