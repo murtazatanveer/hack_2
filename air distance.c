8623f4972215230292995243b644c20abb3fdcea
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-10-20 19:54:18 +0500

    1st commit

---
+++ air distance.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+double lat_of_ori , lon_of_ori , lat_of_des , lon_of_des;
+
+printf("\n\nEnter Latitude and Longitude of Origin ( In Degrees )\n\n");
+
+printf("Latitude : ");
+scanf("%lf",&lat_of_ori);
+
+
+
+
+}
