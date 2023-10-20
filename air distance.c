f95dec9ef9abbe6ca313fd7a05ecca4c8adc7ec7
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-10-20 19:56:37 +0500

    2nd

--- air distance.c
+++ air distance.c
@@ -8,6 +8,25 @@ printf("\n\nEnter Latitude and Longitude of Origin ( In Degrees )\n\n");
 printf("Latitude : ");
 scanf("%lf",&lat_of_ori);
 
+printf("\n\nLongitude : ");
+scanf("%lf",&lon_of_ori);
+
+
+printf("\n\nEnter Latitude and Longitude of Destination ( In Degrees )\n\n");
+
+printf("Latitude : ");
+scanf("%lf",&lat_of_des);
+
+printf("\n\nLongitude : ");
+scanf("%lf",&lon_of_des);
+
+
+
+
+
+
+
+
 
 
 
